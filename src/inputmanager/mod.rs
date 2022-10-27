cc4a013fa116f26706074cd5735a140e032024e4
r59q <andreashenriksen@yahoo.dk>
2022-10-27 10:18:24 +0200

    Started axis inputs

--- src/inputmanager/mod.rs
+++ src/inputmanager/mod.rs
@@ -41,14 +41,34 @@ mod test {
     #[test]
     fn can_add_new_correct_binding() {
         let mut keybindings = Bindings::new();
-        keybindings.bind_key_to_axis(Axis::Horizontal, KeyCode::Char('d'),KeyCode::Char('a'));
-        let binding = keybindings.get_axial_bindings(Axis::Horizontal);
-        let opt_binding = binding.get(0);
 
-        match opt_binding {
+        keybindings.bind_key_to_axis(
+            Axis::Horizontal, 
+            KeyCode::Char('d'),
+            KeyCode::Char('a')
+        );
+        keybindings.bind_key_to_axis(
+            Axis::Vertical, 
+            KeyCode::Char('w'),
+            KeyCode::Char('s')
+        );
+        let binding_horizontal = keybindings.get_axial_bindings(Axis::Horizontal);
+        let binding_vertical = keybindings.get_axial_bindings(Axis::Vertical);
+        let opt_binding_horizontal = binding_horizontal.get(0);
+        let opt_binding_vertical = binding_vertical.get(0);
+
+        match opt_binding_horizontal {
             Some(binding) => {
                 assert_eq!(binding.positive, KeyCode::Char('d'));
                 assert_eq!(binding.negative, KeyCode::Char('a'))
+            },
+            None => assert!(false),
+        }
+
+        match opt_binding_vertical {
+            Some(binding) => {
+                assert_eq!(binding.positive, KeyCode::Char('w'));
+                assert_eq!(binding.negative, KeyCode::Char('s'))
             },
             None => assert!(false),
         }
--- src/resources/mod.rs
+++ src/resources/mod.rs
@@ -1,3 +1,4 @@
 pub mod timer;
 pub mod render_targets;
 pub mod inputs;
+pub mod axis_inputs;
