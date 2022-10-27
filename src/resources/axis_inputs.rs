0e5b77ee94419fb519ab66828ea656f33827089f
r59q <andreashenriksen@yahoo.dk>
2022-10-27 11:50:10 +0200

    Added test for adding key bindings to axis inputs

--- src/resources/axis_inputs.rs
+++ src/resources/axis_inputs.rs
@@ -9,18 +9,33 @@ impl AxisInputs {
     fn get(&self, horizontal: crate::inputmanager::axis::Axis) -> Option<f64> {
         return None;
     }
+
+    fn add_binding(&self, bindings: crate::inputmanager::bindings::Bindings) {
+        todo!()
+    }
 }
 
 #[cfg(test)]
 mod test {
 
+    use console_engine::KeyCode;
+
     use super::AxisInputs;
-    use crate::inputmanager::axis::Axis;
+    use crate::inputmanager::{axis::Axis, bindings, self};
 
     #[test]
     fn can_get_axial_input() {
         let axis_inputs = AxisInputs::new();
 
+        let horizontal_input_value = axis_inputs.get(Axis::Horizontal);
+        assert!(matches!(horizontal_input_value, None))
+    }
+
+    #[test]
+    fn can_add_bindings_to_axis_inputs() {
+        let axis_inputs = AxisInputs::new();
+        let bindings = bindings::Bindings::new();
+        axis_inputs.add_binding(bindings);
         let horizontal_input_value = axis_inputs.get(Axis::Horizontal);
         assert!(matches!(horizontal_input_value, None))
     }
