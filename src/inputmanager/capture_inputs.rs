d5c18cc74e50f1ade6450b5969acbd81384f305a
r59q <andreashenriksen@yahoo.dk>
2022-11-03 12:33:54 +0100

    Capture inputs

--- src/inputmanager/capture_inputs.rs
+++ src/inputmanager/capture_inputs.rs
@@ -1,8 +1,15 @@
-use crate::game::Game;
+use crate::{game::Game, resources::mouse_inputs::{self, MouseInputs}};
 
 pub fn capture_inputs(game: &mut Game) {
+    let mouse_inputs_opt = game.get_world_mut().get_resource_mut::<MouseInputs>();
     let engine = game.get_engine();
+    let m_left_held = engine.get_mouse_held(console_engine::MouseButton::Left);
 
-    // engine.get_mouse_held(console_engine::MouseButton::Left);
+    if let Some(mouse_inputs) = mouse_inputs_opt {
+
+    } else {
+        panic!("NO MOUSE INPUTS WERE AVAILABLE FOR CAPTURE")
+    }
+
     todo!();
 }
