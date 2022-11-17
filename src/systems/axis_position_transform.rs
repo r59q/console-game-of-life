f6cdffca13b51873a360f665af9542c98d76c9f6
r59q <andreashenriksen@yahoo.dk>
2022-11-17 11:25:59 +0100

    Transform axis system no longer moves entity if pause state is paused

--- src/systems/axis_position_transform.rs
+++ src/systems/axis_position_transform.rs
@@ -4,8 +4,12 @@ use crate::components::controllable::Controllable;
 use crate::components::position::Position;
 use crate::input_manager::axis::Axis::{Horizontal, Vertical};
 use crate::resources::inputs::axis_inputs::AxisInputs;
+use crate::resources::pause_state::PauseState;
 
-pub fn axis_position_transform(mut query: Query<&mut Position, With<Controllable>>, axis_inputs :Res<AxisInputs>) {
+pub fn axis_position_transform(mut query: Query<&mut Position, With<Controllable>>, axis_inputs :Res<AxisInputs>, pause_state: Res<PauseState>) {
+    if pause_state.is_paused() {
+        return;
+    }
     for mut position in &mut query {
         position.x += axis_inputs.get(&Horizontal);
         position.y += axis_inputs.get(&Vertical);
