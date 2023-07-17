e97c4447890e1d70d9ee061adfecf3ee046fe22f
A. Malthe Henriksen <andreashenriksen@yahoo.dk>
2023-07-17 16:41:44 +0200

    Added toggleling of cells

--- src/systems/toggle_cell_on_click.rs
+++ src/systems/toggle_cell_on_click.rs
@@ -18,16 +18,26 @@ pub fn toggle_cell_on_click(
     mut positioned_entities: ResMut<PositionedEntities>,
     mut commands: Commands,
 ) {
-    let should_spawn_cell =
-        button_inputs.get_btn_action(Button::Fire1).clone() == InputAction::Down;
+    let did_press_button = button_inputs.get_btn_action(Button::Fire1).clone() == InputAction::Down;
+    let target_pos = view_offset.get_mouse_to_world_offset_i32(mouse_inputs.get_position());
 
-    if should_spawn_cell {
-        let target_pos = view_offset.get_mouse_to_world_offset_i32(mouse_inputs.get_position());
+    let new_cell_pos_component = Position {
+        x: target_pos.0 as f64,
+        y: target_pos.1 as f64,
+    };
 
-        let new_cell_pos_component = Position {
-            x: target_pos.0 as f64,
-            y: target_pos.1 as f64,
-        };
+    let cell_already_exists = positioned_entities.get(&target_pos).is_some();
+
+    if !did_press_button {
+        return;
+    }
+
+    if cell_already_exists {
+        commands
+            .entity(*positioned_entities.get(&target_pos).unwrap())
+            .despawn();
+        positioned_entities.remove_entity(target_pos);
+    } else {
         let placed_entity =
             Prefabs::CELL_WITHOUT_POSITION(&mut commands.spawn().insert(new_cell_pos_component));
         positioned_entities.put_entity(placed_entity, new_cell_pos_component);
--- src/tests/spawn_cell_on_click_test.rs
+++ src/tests/spawn_cell_on_click_test.rs
@@ -54,3 +54,47 @@ fn down_input_places_cell() {
     assert!(test_entity.is_some());
     assert!(positioned_entities.get_all_entities().len() == 1);
 }
+
+#[test]
+fn can_toggle_cells() {
+    let mut test_env = initialize();
+
+    let mut view_offset = ViewOffset::new();
+    view_offset.set_offset(10, 10);
+
+    test_env.game.get_world_mut().insert_resource(view_offset);
+    test_env
+        .game
+        .get_world_mut()
+        .insert_resource(PositionedEntities::new());
+
+    let mut button_inputs = ButtonInputs::new();
+    button_inputs.set_btn(Button::Fire1, InputAction::Down);
+
+    test_env.game.get_world_mut().insert_resource(button_inputs);
+
+    let mut mouse_inputs = MouseInputs::new();
+    mouse_inputs.set_state(MouseButton::Left, InputAction::Down, 1, 1);
+    test_env.game.get_world_mut().insert_resource(mouse_inputs);
+
+    test_env.game.add_stage_to_schedule(
+        "stage",
+        SystemStage::parallel().with_system(toggle_cell_on_click),
+    );
+
+    test_env.game.run_schedule();
+    test_env.game.run_schedule(); // Running twice is like double clicking
+
+    let positioned_entities = test_env
+        .game
+        .get_world_ref()
+        .get_resource::<PositionedEntities>()
+        .unwrap();
+
+    let test_entity = positioned_entities.get(&(11, 11));
+
+    assert!(test_entity.is_none());
+    assert!(positioned_entities.get_all_entities().len() == 0);
+
+    // Add ability to toggle cell
+}
